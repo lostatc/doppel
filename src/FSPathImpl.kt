a90318d51a500ab6589962467f491ef979f5fe40
Garrett Powell <garrett@gpowell.net>
2018-05-22 09:29:42 -0400

    Move main function to separate file
    Because this function is only used for testing, changes to it aren't
    important to track. Moving it to a separate file ensures that the output
    of `git diff` shows changes to it separately.

--- src/FSPathImpl.kt
+++ src/FSPathImpl.kt
@@ -286,36 +286,3 @@ class FilePathView(inner: FilePathBase) : FilePathBase by inner
  * objects are updated, this view reflects those changes. View objects cannot modify their corresponding path objects.
  */
 class DirPathView(inner: DirPathBase) : DirPathBase by inner
-
-fun main(args: Array<String>) {
-//    val dir1 = DirPath.of("/", "home", "garrett")
-//    dir1.addChildren(
-//        DirPath.of("Documents",
-//            FilePath.of("Concepts Essay.odt"),
-//            FilePath.of("Innovation Essay.odt")
-//        ),
-//        DirPath.of("Music",
-//            DirPath.of("Popular Band",
-//                FilePath.of("Song 1.mp3"),
-//                FilePath.of("Song 2.mp3")
-//            )
-//        )
-//    )
-//
-//    val dir2 = DirPath.of("/", "home", "garrett")
-//    dir2.addChildren(
-//        DirPath.of("Documents",
-//            FilePath.of("Rhetoric Essay.odt"),
-//            FilePath.of("Concepts Essay.odt")
-//        ),
-//        DirPath.of("Music",
-//            DirPath.of("Popular Band",
-//                FilePath.of("Song 1.mp3"),
-//                FilePath.of("Song 3.mp3")
-//            ),
-//            DirPath.of("Obscure Band",
-//                FilePath.of("Song 1.mp3")
-//            )
-//        )
-//    )
-}
---
+++ src/Main.kt
@@ -0,0 +1,34 @@
+package diffir
+
+fun main(args: Array<String>) {
+//    val dir1 = DirPath.of("/", "home", "garrett")
+//    dir1.addChildren(
+//        DirPath.of("Documents",
+//            FilePath.of("Concepts Essay.odt"),
+//            FilePath.of("Innovation Essay.odt")
+//        ),
+//        DirPath.of("Music",
+//            DirPath.of("Popular Band",
+//                FilePath.of("Song 1.mp3"),
+//                FilePath.of("Song 2.mp3")
+//            )
+//        )
+//    )
+//
+//    val dir2 = DirPath.of("/", "home", "garrett")
+//    dir2.addChildren(
+//        DirPath.of("Documents",
+//            FilePath.of("Rhetoric Essay.odt"),
+//            FilePath.of("Concepts Essay.odt")
+//        ),
+//        DirPath.of("Music",
+//            DirPath.of("Popular Band",
+//                FilePath.of("Song 1.mp3"),
+//                FilePath.of("Song 3.mp3")
+//            ),
+//            DirPath.of("Obscure Band",
+//                FilePath.of("Song 1.mp3")
+//            )
+//        )
+//    )
+}
