0bd4a8879901f88c9218a949aab54163fb2634f2
Garrett Powell <garrett@gpowell.net>
2018-11-23 18:33:40 -0500

    Resolve dependency issues

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,8 @@ val test by tasks.getting(Test::class) {
 
 dependencies {
     compile(kotlin("stdlib"))
+    testCompile(kotlin("reflect"))
+    testCompile(kotlin("stdlib-jdk7"))
     testCompile(group = "io.kotlintest", name = "kotlintest-runner-junit5", version = "3.1.10")
     testCompile(group = "com.google.jimfs", name = "jimfs", version = "1.1")
 }
